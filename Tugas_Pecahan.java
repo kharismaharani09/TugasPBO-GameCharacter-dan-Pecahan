b5d0f95b258c44c0c177a02c0903ed3ef0942fda
kharismaharani09 <56229343+kharismaharani09@users.noreply.github.com>
2020-11-17 21:52:24 +0700

    Update Tugas_Pecahan.java

--- Tugas_Pecahan.java
+++ Tugas_Pecahan.java
@@ -1,52 +1,39 @@
-import java.util.*;
 class Pecahan
 {
-    Scanner inputUser = new Scanner(System.in);
-    int atas, bawah,  pembilang1, penyebut1, pembilang2, penyebut2;
-    Pecahan()
+    int newpembilang, newpenyebut, pembilang, penyebut, pembilang2, penyebut2;
+    Pecahan(int inputPembilang, int inputPenyebut)
     {
-        System.out.println("----PENJUMLAHAN DUA PECAHAN----");
-        System.out.print("Pembilang : ");
-        pembilang1 = inputUser.nextInt();
-        System.out.print("Penyebut : ");
-        penyebut1 = inputUser.nextInt();
-        System.out.println("Pecahan ke-1 : " + pembilang1 + "/" + penyebut1 + "\n");
-
-        System.out.print("Pembilang : ");
-        pembilang2 = inputUser.nextInt();
-        System.out.print("Penyebut : ");
-        penyebut2 = inputUser.nextInt();
-        System.out.println("Pecahan ke-2 : " + pembilang2 + "/" + penyebut2 + "\n" );
-
-        tambah();
+        this.pembilang = inputPembilang;
+        this.penyebut = inputPenyebut;
     }
     public int getPembilang()
     {
-        return atas;
+        return newpembilang;
     }
 
     public int getPenyebut()
     {
-        return bawah;
+        return newpembilang;
     }
 
-    public void tambah()
+    public void tambah(int pembilang2, int penyebut2)
     {
-        if (penyebut1 == penyebut2)
-        {
-            atas = pembilang1 + pembilang2;
-            bawah = penyebut1;
-            System.out.println("Hasil Penjumlahan Pecahan = " + atas + "/" + bawah);
-        }
-        else
-        {
-            atas = (penyebut1*pembilang2) + (penyebut2*pembilang1);
-            bawah = penyebut1*penyebut2;
-            System.out.println("Hasil Penjumlahan Pecahan = " + atas + "/" + bawah);
-        }
+        newpembilang = (penyebut*pembilang2) + (penyebut2*pembilang);
+        newpenyebut = penyebut*penyebut2;
+        System.out.println(pembilang + "/" + penyebut + " + " + pembilang2 + "/" + penyebut2 + " = " + newpembilang + "/" + newpenyebut);
     }
+}
+
+public class Tugas_Pecahan
+{
     public static void main(String[] args) 
     {
-        Pecahan obj = new Pecahan();
+        System.out.println("---PENJUMLAHAN DUA PECAHAN---");
+        System.out.println("Pecahan 1 = 3/2");
+        System.out.println("Pecahan 2 = 5/6");
+        Pecahan obj1 = new Pecahan(3, 2);
+        Pecahan obj2 = new Pecahan(5, 6);
+
+        obj1.tambah(5,6);
     }
 }
